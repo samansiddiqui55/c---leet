65a660cf09a7dd62004d1745146d678b314d0551
samansiddiqui55 <114631933+samansiddiqui55@users.noreply.github.com>
2024-07-14 18:02:49 +0530

    Update missing_number.cpp

--- ARRAY/missing_number.cpp
+++ ARRAY/missing_number.cpp
@@ -11,3 +11,22 @@ int main() {
    sum2 = (5*(5+1))/2; 
    cout <<(sum2-sum1); 
 }
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std ; 
+int main(){
+int n; 
+cout <<" enter the size of n"<<endl; 
+  cin>>n; 
+  int arr[n] ,sum1=0,sum2=0; 
+for (int i = 0 ; i <n; i++){
+  cin>>arr[i]; 
+}
+  for (int i = 0 ; i <n ; i++){
+    sum2 = sum2 +arr[i]; 
+  }
+  for(int i = 1 ; i <=n+1; i++){
+    sum1= sum1+ i;
+  }
+  cout <<"missing number"<<sum1-sum2<<endl; 
+}
